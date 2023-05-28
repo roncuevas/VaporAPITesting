2ae121cf5f8fde0fdf98518122dc80614413a36f
Aarón Martínez Cuevas <ronctm@gmail.com>
2023-05-28 13:53:25 -0600

    UPDATE: Adding randomUser route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,12 +3,18 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        req.description
     }
 
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
-
+    
+    app.get("randomuser") { req in
+        req.client.get(URI(string: "https://randomuser.me/api/")).flatMap { response in
+            return response.encodeResponse(for: req)
+        }
+    }
+    
     try app.register(collection: TodoController())
 }
